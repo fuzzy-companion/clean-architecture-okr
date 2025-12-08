26f991f15052b0016fc6c8940ccad16c75a6b81a
OKBenzene02 <liyakhatyousuf@gmail.com>
2025-12-08 23:53:08 +0530

    changed the folder structure and updated langchain to v3 and updated methods

--- src/vscode_ext.js
+++ src/vscode_ext.js
@@ -49,7 +49,7 @@ function activate(context) {
           return;
         }
 
-        const rootPath = workspaceFolders[0].uri.fsPath;
+        const rootPath = workspaceFolders[0].uri.fsPath / 'lib' / 'features';
         
         files.forEach((file) => {
           const filePath = path.join(rootPath, file.path);
